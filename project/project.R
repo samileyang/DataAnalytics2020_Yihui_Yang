09e248d58bd208252b9072ea86d355672fc7751a
Yihui Yang <35894512+samileyang@users.noreply.github.com>
2020-11-28 00:23:58 -0500

    Add files via upload

--- project/project.R
+++ project/project.R
@@ -27,6 +27,7 @@ df[df$SO2 == 0,]$SO2 <-mean(df$SO2)
 df[df$CO == 0,]$CO <-mean(df$CO)
 df[df$NO2 == 0,]$NO2 <-mean(df$NO2)
 df[df$O3_8h == 0,]$O3_8h <-mean(df$O3_8h)
+summary(df)
 
 ggplot(df, aes(x=Datetime, y=AQI)) +
   geom_line()
@@ -78,10 +79,14 @@ print(km.res)
 km.res.agg <- aggregate(df_heatmap,by=list(km.res$cluster),mean)
 km.res.agg
 
-df$kmean <- km.res$cluster
-df
-
-
+df$kmean <- as.numeric(km.res$cluster)
+length(df$kmean)
+kmeans_df <- subset(df,select = -c(Datetime))
+kmeans_df_agg <- aggregate(kmeans_df,by=list(kmeans_df$kmean),mean)
+kmeans_df_agg
+kmeans_df_count <- aggregate(kmeans_df,by=list(kmeans_df$kmean),count)
+kmeans_df_count
+df$kmean <- as.factor(km.res$cluster)
 b <- ggplot(df, aes(x = Datetime, y = AQI))
 b + geom_point(aes(color = kmean))
 
@@ -107,3 +112,37 @@ res_18_19
 res_cov <- t.test(df_2019_during_cov_period$AQI,df_2020_during_cov$AQI,var.equal = TRUE,alternative = "greater")
 res_cov
 
+ts_df <- df[,c("Datetime","AQI")]
+library(forecast)
+aqi.ts <- ts(ts_df$AQI,start = c(2016,1,1),end = c(2019,12,31),freq=30)
+aqi.ts
+
+# plot the series
+# the bty option allows to custom the box around the plot
+plot(aqi.ts,xlab="Time",ylab="AQI")
+nValid <- 5
+nTrain <- length(aqi.ts)-nValid
+train.ts <- window(aqi.ts, start = c(2016,1), end = c(2016,nTrain))
+valid.ts <- window(aqi.ts,start = c(2016,nTrain+1),end=c(2016,nTrain+nValid))
+
+train.lm <- tslm(train.ts~trend)
+train.lm.pred <- forecast(train.lm, h=nValid, level=0)
+
+plot(train.lm.pred,ylab="AQI",xlab="Time",
+     xaxt="n")
+
+lines(train.lm.pred$fitted,lwd=2,col="blue")
+lines(valid.ts)
+
+summary(train.lm)
+train.lm.season <- tslm(train.ts~season)
+summary(train.lm.season)
+train.lm.trend.season <- tslm(train.ts~trend+I(trend^2)+season)
+summary(train.lm.trend.season)
+train.lm.pred <- forecast(train.lm.trend.season, h=nValid, level=0)
+plot(train.lm.pred,ylab="AQI",xlab="Time",
+     xaxt="n",lty = 1)
+
+lines(train.lm.pred$fitted,lwd=3,col="red",lty = 5)
+lines(valid.ts)
+accuracy(train.lm.pred,valid.ts)
